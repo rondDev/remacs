87ebbeb3e5c61c92c8b75cf2369a9a746b1a7966
rond <contact@rond.cc>
2024-03-02 13:38:47 +0100

    fix: search and replace oopsie

--- modules/tools/magit/packages.el
+++ modules/tools/magit/packages.el
@@ -6,7 +6,7 @@
     (package! forge :pin "3fc6c362b0162082317c128c9c3226529f6965ae")
     (package! code-review
       :recipe (:host github
-               :repo "rmcselpa/code-review"
+               :repo "doomelpa/code-review"
                :files ("graphql" "code-review*.el"))
       :pin "2670a4beb6636e6ee596c5b7cb5e784cf33d5a98"))
   (package! magit-todos :pin "debb77b3589f2d83c8b43706edc1f8f90bf1ad91"))
