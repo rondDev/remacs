6597b4916f5a31d9c427425f480f1ef458bc8426
rond <contact@rond.cc>
2024-03-27 21:46:02 +0100

    fix: bump apheleia

--- modules/editor/format/packages.el
+++ modules/editor/format/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; editor/format/packages.el
 
-(package! apheleia :pin "96a9805ecb75aac2adde7568d26b3e3b3ffc19af")
+(package! apheleia :pin "3e89e62922d610a3429707710d6636244a3c3f76")
