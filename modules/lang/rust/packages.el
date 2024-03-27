2a52d7bb33effb905723b68ccc3242908a4e8640
rond <contact@rond.cc>
2024-03-27 21:45:51 +0100

    fix: bump rust-mode

--- modules/lang/rust/packages.el
+++ modules/lang/rust/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; lang/rust/packages.el
 
-(package! rust-mode :pin "f74dd1cd87987ea7faf0cfc6240c2284ef9133cb")
+(package! rust-mode :pin "150b61f7ed9593c1d433794aa097f2e9ca8cdf75")
 (package! rustic :pin "39423d1cf4fa054c36bf9577356451f4c06ee148")
