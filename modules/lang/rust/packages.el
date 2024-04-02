c4f39bc84caf4464503d4f554600cf6932d9280a
rond <contact@rond.cc>
2024-04-02 15:08:22 +0200

    feat: add rustic popup

--- modules/lang/rust/packages.el
+++ modules/lang/rust/packages.el
@@ -3,3 +3,5 @@
 
 (package! rust-mode :pin "150b61f7ed9593c1d433794aa097f2e9ca8cdf75")
 (package! rustic :pin "39423d1cf4fa054c36bf9577356451f4c06ee148")
+                                        ; TODO: remove this
+(package! parsec)
